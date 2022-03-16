aa7bd2da778fb1b3384d6f021f19991dc1f65275
Micaela Pucciariello <pucciariellomicaela@gmail.com>
2022-03-15 23:43:17 -0300

    added get all method

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyparser = require("body-parser");
+const ejs = require('ejs');
+
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+mongoose.connect("mongodb://localhost:27017/mydb");
+
+const schema = {
+  tittle: String,
+  description: String,
+};
+
+const Food = mongoose.model("article", schema);
+
+app.listen(3000, function(){
+    console.log("server up and running!");
+});
+
+app.get("/", function (request, response) {
+  Food.find(function (err, articles) {
+    if (err == null) {
+      response.send(JSON.stringify(articles));
+    } else {
+      response.send(err);
+    }
+  });
+});
